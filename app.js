90b783874e8e52d9f2f67554c3910355d6c07006
Vansh Sharma <vanshsharma9354@gmail.com>
2021-05-30 15:02:08 +0530

    style: color ,font
    Added background color, font color , borders

--- app.js
+++ app.js
@@ -1,37 +1,34 @@
-var translatebtn=document.querySelector("#translate-button");
-var txtInput=document.querySelector("#txt-translate");
-var outputDiv=document.querySelector("#output");
+var translatebtn = document.querySelector("#translate-button");
+var txtInput = document.querySelector("#txt-translate");
+var outputDiv = document.querySelector("#output");
 
+// API
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
-
-var serverURL="https://api.funtranslations.com/translate/minion.json"
-
-
-function getTranslationURL(input){
- return serverURL + "?" + "text="+ input
+function getTranslationURL(input) {
+  return serverURL + "?" + "text=" + input;
 }
 
-function errorHandler(error)
-{
- console.log("error occured", error);
- alert("Sorry! Try after sometime.")
 
+// Error function
+function errorHandler(error) {
+  console.log("error occured", error);
+  alert("Sorry! Try after sometime.");
 }
 
-function clickHandler()
-{
-var inputText=txtInput.value;
 
-fetch(getTranslationURL(inputText))
- .then(response => response.json())
- .then(json =>
- {
+// Function to take input from URL and convert it to language
+function clickHandler() {
+  var inputText = txtInput.value;
 
-  var translatedText=json.contents.translated;
-  outputDiv.innerText=translatedText;
+  fetch(getTranslationURL(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler);
+}
 
- })
-.catch(errorHandler)
-};
-
-translatebtn.addEventListener("click", clickHandler)
+// Event after click
+translatebtn.addEventListener("click", clickHandler);
