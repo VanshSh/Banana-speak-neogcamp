a8877a2b2d88770c5c80c9e648ba44122eac85de
VanshSh <vanshsharma9354@gmail.com>
2022-01-28 22:06:12 +0530

    Refact: Refactored the old code Description: Used Let instead of Var. Changes the Readme.

--- README.md
+++ README.md
@@ -1,24 +1,29 @@
 <div align="center"><h1> 🍌 Banana Language 🍌 </h1></div>
 
-<p> If you are Minion fan like me. </p>
-<p> You might be fascinated by the "Banana Language " used by minions. </p>
-<p> You might also wants to know <br/>
-  What they Speak ? <br/> 
-  How they speak ? <br/> </p>
-  
-<p>Don't worry. Here is <b> Minion Speak </b> translator for your help. Now translate in "Banana Language" and speak like Minions. </p>  
+If you are minion lover then this app is for you.
+It translates the input given by user to the **_Minion Language_**.
 
+This app use simple [Fun translation API](https://api.funtranslations.com/) which helps in translating the language.
 
-<img src="https://user-images.githubusercontent.com/81517284/121346564-e5c7cb00-c943-11eb-8120-ebc7797f1950.gif" width="950px">
+## [LIVE APP]()
+---
+![Demo Image](./images/demo.png)
 
-<h3> Use this translator :point_down:</h3> <a href="https://bananalangage.netlify.app/" target="_blank">Banana Language</a>
+---
 
-
-
-<h3> Language used: </h3>
+<h3> Tech used: </h3>
 1. HTML<br/>
 2. CSS<br/>
 3. JavaScript<br/>
+4. API
 
+---
 
-  
+> NOTE: This API allows only 5 request in an hour. 
+
+---
+
+Contact :
+
+- [Twitter](https://twitter.com/Vanshsh2701)
+- [Linkedin](https://www.linkedin.com/in/vanshsharma27/)
--- app.js
+++ app.js
@@ -1,34 +1,21 @@
-var translatebtn = document.querySelector("#translate-button");
-var txtInput = document.querySelector("#txt-translate");
-var outputDiv = document.querySelector("#output");
+const inputTxt = document.getElementById('inputTxt')
+const btn = document.getElementById('btn')
+const outputTxt = document.getElementById('outputTxt')
 
 // API
-var serverURL = "https://api.funtranslations.com/translate/minion.json";
+const URL = 'https://api.funtranslations.com/translate/minion.json'
 
-function getTranslationURL(input) {
-  return serverURL + "?" + "text=" + input;
-}
+btn.addEventListener('click', translateText)
 
-
-// Error function
-function errorHandler(error) {
-  console.log("error occured", error);
-  alert("Sorry! Try after sometime.");
-}
-
-
-// Function to take input from URL and convert it to language
-function clickHandler() {
-  var inputText = txtInput.value;
-
-  fetch(getTranslationURL(inputText))
+function translateText() {
+  let newURL = `${URL}?text=${inputTxt.value}`
+  fetch(newURL)
     .then((response) => response.json())
-    .then((json) => {
-      var translatedText = json.contents.translated;
-      outputDiv.innerText = translatedText;
-    })
-    .catch(errorHandler);
+    .then((data) => (outputTxt.innerHTML = data.contents.translated))
+    .catch(errorHandler)
 }
 
-// Event after click
-translatebtn.addEventListener("click", clickHandler);
+function errorHandler(error) {
+  console.log('error occured', error.message)
+  alert('Please! Try after 1 hour')
+}
